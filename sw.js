67b37d788420607c5bdddb3586697e45b60e5280
unknown <iy1573147@163.com>
2019-04-13 19:37:52 +0800

    Site updated: 2019-04-13 19:37:48

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","ccaad41c8af3555758c474d8e9f0faba"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
